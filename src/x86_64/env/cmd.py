014132230ca802531851449f449fbc819a2e5180
DrxcoDev <drxcovalorant@gmail.com>
2024-02-09 19:58:45 +0100

    New Update :)

--- src/x86_64/env/cmd.py
+++ src/x86_64/env/cmd.py
@@ -6,5 +6,15 @@ colorama.init()
 
 os.system("cls")
 
-print(colorama.Fore.YELLOW + "Hola Mundo")
-input("~ User $ ")
+print(colorama.Fore.YELLOW + "[Vymi console || 2024 Copyright || v.1.0]")
+while True:
+    root = input("~ User $ ")
+
+    if root == "!clear":
+        os.system("cls")
+        time.sleep(1)
+    
+    elif root == "!exit" or root == "!quit":
+        quit()
+        
+
