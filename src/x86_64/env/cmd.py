de4ffe0b46a52e95000092794d4cd23fb1d13fd1
DrxcoDev <drxcovalorant@gmail.com>
2024-02-09 20:31:53 +0100

    Update :)

--- src/x86_64/env/cmd.py
+++ src/x86_64/env/cmd.py
@@ -1,6 +1,7 @@
 import os
 import time
 import colorama
+import subprocess
 colorama.init()
 
 
@@ -16,5 +17,11 @@ while True:
     
     elif root == "!exit" or root == "!quit":
         quit()
+
+    elif root == "!new cmd":
+        if os.path.exists("src/x86_64/env/util/NewCmd.exe"):
+            subprocess.run([r"C:/Users/Soft/Documents/Py/src/x86_64/env/util/NewCmd.exe"], shell=True)
+        else:
+            os.system("g++ src/x86_64/env/util/NewCmd.cpp -o src/x86_64/env/util/NewCmd.exe")
         
 
---
+++ src/x86_64/env/util/NewCmd.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cstdlib> // Para la función system
+
+int main() {
+    // Comando para ejecutar un archivo de Python (reemplaza "script.py" con tu nombre de archivo)
+    const char* comando = "python src/x86_64/env/cmd.py";
+
+    // Ejecutar el comando
+    int resultado = system(comando);
+
+    // Verificar si la ejecución fue exitosa
+    if (resultado == 0) {
+        std::cout << "El archivo de Python se ejecutó correctamente." << std::endl;
+    } else {
+        std::cout << "Ocurrió un error al ejecutar el archivo de Python." << std::endl;
+    }
+
+    return 0;
+}
